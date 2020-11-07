6328d3b2ab2fdab33cc0c946dee4d09900082e1c
emmanuel-karanja <60355631+emmanuel-karanja@users.noreply.github.com>
2020-11-07 06:46:53 +0300

    Added career-day-server

---
+++ career-day-server/career-day-app/src/main/java/com/careerday/careerdayapp/CareerDayAppApplication.java
@@ -0,0 +1,13 @@
+package com.careerday.careerdayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerDayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerDayAppApplication.class, args);
+	}
+
+}
