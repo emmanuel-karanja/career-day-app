72877d21cab70f2cac301358acae290a2141cb13
emmanuel-karanja <60355631+emmanuel-karanja@users.noreply.github.com>
2020-11-09 21:20:38 +0300

    added JobController integration test

--- career-day-server/career-day-app/src/test/java/com/careerday/tests/controllers/JobControllerTests.java
+++ career-day-server/career-day-app/src/test/java/com/careerday/tests/controllers/JobControllerTests.java
@@ -1,71 +1,121 @@
 package com.careerday.tests.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.careerday.careerdayapp.Entities.Job;
-import com.careerday.careerdayapp.Entities.JobTypeName;
-import com.careerday.careerdayapp.Entities.LevelOfEducation;
-import com.careerday.careerdayapp.Services.JobService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.careerday.careerdayapp.CareerDayAppApplication;
+import org.junit.runners.MethodSorters;
 
+@RunWith(SpringRunner.class)
+@TestPropertySource(
+		  locations = "classpath:application-integrationtest.properties")
+@ContextConfiguration(classes=CareerDayAppApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@AutoConfigureMockMvc
+@SpringBootTest
 class JobControllerTests {
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
-	private JobService jobService;
-	
 	@Autowired
-	private ObjectMapper objectMapper;
+	private WebApplicationContext wac;
 	
-	private List<Job> jobList;
-
-	@BeforeEach
-	void setUp() throws Exception {
-		final Job job=new Job();
-		job.setName("UI Dude");
-		job.setDescription("We want a front-end ninja with at least 3 years experience in React and Redux");
-		job.setSummary("Front-end Ninja wanted");
-		job.setInterviewAt(LocalDate.of(2020, 12, 21));
-		job.setInterviewStartTime(LocalTime.of(10, 00));
-		job.setInterviewEndTime(LocalTime.of(17, 00));
-		job.setLevelOfEducation(LevelOfEducation.GRADUATE);
-		job.setType(JobTypeName.UI_ENGINEER);
-		
-		final Job job1=new Job();
-		job1.setName("API Guy");
-		job1.setDescription("We want a front-end ninja with at least 3 years experience in React and Redux");
-		job1.setSummary("Front-end Ninja wanted");
-		job1.setInterviewAt(LocalDate.of(2020, 12, 12));
-		job1.setInterviewStartTime(LocalTime.of(10, 00));
-		job1.setInterviewEndTime(LocalTime.of(17, 00));
-		job1.setLevelOfEducation(LevelOfEducation.POST_GRADUATE);
-		job1.setType(JobTypeName.API_ENGINEER);
-		
-		this.jobList=new ArrayList<>();
-		
-		this.jobList.add(job);
-		this.jobList.add(job1);
-		
-		
-		
+	@Before
+	public void setup() {
+		this.mockMvc=MockMvcBuilders.webAppContextSetup(wac).build();
 	}
-
+	/*@Test
+	public void a_releteJobSuccessful() throws Exception{
+		mockMvc
+		    .perform(MockMvcRequestBuilders.delete("/api/v1/jobs/1").accept(MediaType.APPLICATION_JSON))
+		    .andExpect(status().isOk())
+		    .andExpect(jsonPath("$.success").value("true"))
+		    .andExpect(jsonPath("$.message").exists())
+		    .andDo(print());
+	}*/
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getAllJobs() throws Exception{
+		mockMvc
+		   .perform(MockMvcRequestBuilders.get("/api/v1/jobs").accept(MediaType.APPLICATION_JSON))
+		   .andExpect(status().isOk())
+		   .andExpect(jsonPath("$",hasSize(1)))
+		   .andDo(print());
 	}
-
+	
+	@Test
+	public void getJobById() throws Exception{
+		mockMvc
+		    .perform(MockMvcRequestBuilders.get("/api/v1/jobs/1").accept(MediaType.APPLICATION_JSON))
+		    .andExpect(status().isOk())
+		    .andExpect(jsonPath("$.name").exists())
+			.andExpect(jsonPath("$.status").exists())
+			.andExpect(jsonPath("$.summary").exists())
+			.andExpect(jsonPath("$.type").exists())
+		    .andDo(print());
+		    
+	}
+	
+	@Test
+	public void getJobByInvalidId() throws Exception{
+		mockMvc
+		  .perform(MockMvcRequestBuilders.get("/api/v1/jobs/17").accept(MediaType.APPLICATION_JSON))
+		  .andExpect(status().isNotFound())
+		  .andExpect(jsonPath("$.success").value("false"))
+		  .andExpect(jsonPath("$.message").exists())
+		  .andDo(print());
+	}
+	
+	@Test
+	public void ZZ_createJob() throws Exception{
+		mockMvc
+		  .perform(MockMvcRequestBuilders.post("/api/v1/jobs")
+				  .contentType(MediaType.APPLICATION_JSON)
+				  .content("{\"name\":\"Serverless Engineer\",\"description\":\"A developer with at least 3 years experience developing GraphQL APIs in Node.js with Claudia.js\",\"summary\":\"Summary of Job 1\",\"jobType\":\"API_ENGINEER\",\"interviewDate\":\"2021-01-11\",\"startTime\":\"10:00\",\"endTime\":\"17:00\",\"levelOfEducation\":\"GRADUATE\"}")
+				  .accept(MediaType.APPLICATION_JSON))
+		  .andExpect(status().isCreated())
+		  .andExpect(jsonPath("$.name").exists())
+		  .andExpect(jsonPath("$.status").exists())
+		  .andExpect(jsonPath("$.summary").exists())
+		  .andExpect(jsonPath("$.type").exists())
+		  .andDo(print());
+		          
+	}
+	
+	
+	@Test
+	public void aa_createJobDuplicate() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/jobs")
+			   .contentType(MediaType.APPLICATION_JSON)
+			   .content("{\"name\":\"Serverless Engineer\",\"description\":\"A developer with at least 3 years experience developing GraphQL APIs in Node.js with Claudia.js\",\"summary\":\"Summary of Job 1\",\"jobType\":\"API_ENGINEER\",\"interviewDate\":\"2021-01-11\",\"startTime\":\"10:00\",\"endTime\":\"17:00\",\"levelOfEducation\":\"GRADUATE\"}")
+			   .accept(MediaType.APPLICATION_JSON))
+		   .andExpect(status().isConflict())
+		   .andExpect(jsonPath("$.success").value("false"))
+		   .andExpect(jsonPath("$.message").exists())
+		   .andDo(print());
+	}
+	
+	
+	
 }
