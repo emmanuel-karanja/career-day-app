473989524c1e1a07a274ce1b1386ddcd2128c438
emmanuel-karanja <60355631+emmanuel-karanja@users.noreply.github.com>
2020-11-09 08:44:54 +0300

    fixed issues with springfox-swagger API documentation

--- career-day-server/career-day-app/src/main/java/com/careerday/careerdayapp/Config/SwaggerConfig.java
+++ career-day-server/career-day-app/src/main/java/com/careerday/careerdayapp/Config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package com.careerday.careerdayapp.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -10,29 +12,29 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(regex("/api/v1/*.*"))                          
-          .build();                                           
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.careerday.careerdayapp.Controllers"))
+                .paths(PathSelectors.any())
+                .build();
     }
-	
-   private ApiInfo metaData() {
-	   ApiInfo apiInfo=new ApiInfo(
-			   "Spring Boot REST API",
-			   "Spring Boot REST API for Online Job Posting",
-			   "1.0",
-			   "Terms of Service",
-			   new Contact("Emmanuel Karanja","existentialkaranja@gmail.com","Job Posting API"),
-					   "Apache Licence 2.0",
-					   "https://www.apache.org.licenses/LICENCE-2.0");
-	   return apiInfo;
-   }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Emmanuel Karanja", "https://www.linkedin.com/in/emmanuel-ng-ang-a-ab868a1a3/", "existentialkaranja@gmail.com");
+        return new ApiInfoBuilder()
+                .title("CareerDay-Job Posting App")
+                .description("CareerDay Api Definition")
+                .version("1.0.0")
+                .license("MIT")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
+    }
 }
